0273bf81a2d6cbcde33fb48459b12a59fc080aec
Shwetanlondhe24 <shwetan.college@gmail.com>
2025-02-17 18:10:46 +0530

    printing pairs in an array

---
+++ arrays/pairs_in_array.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class pairs_in_array {
+    public static void main(String args[]){
+
+        int arr[]={1,2,3,4,5,6,7};
+        int tp = 0;
+
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                System.out.print("("+arr[i] +","+ arr[j] +")" +" ");
+                tp++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("Total number of pairs are: "+ tp);
+    }
+}
+
+// *total pairs = n(n-1)/ 2
